c024fe91226c657389fa7417e5aabe756b0b777b
Elvis Mwenda <ekithinji@dvt.co.za>
2022-09-24 16:57:45 +0300

    Top and bottom

--- Shared/ToolTipShape/SampleToolTipShape.swift
+++ Shared/ToolTipShape/SampleToolTipShape.swift
@@ -10,44 +10,56 @@ public struct SampleToolTipShape: Shape {
     public func path(in rect: CGRect) -> Path {
         let newOrigin = CGPoint(x: rect.origin.x + toolWidth, y: rect.origin.y + toolTipHeight)
         let newRect = CGRect(origin: newOrigin, size: .init(width: rect.width - toolWidth * 2, height: rect.height - toolTipHeight * 2))
-        var newPath = Path()
+        var path = Path()
         switch toolTipPosition {
         case .top(let xMultiplier, let xConstant):
             let xPosition = CGFloat(round(newRect.maxX - (newRect.maxX * xMultiplier)) - radius / 2)
-            newPath.addRelativeArc(center: .init(x: newOrigin.x + radius, y: newOrigin.y + radius), radius: radius, startAngle: .degrees(180), delta: .degrees(90))
+            path.addRelativeArc(center: .init(x: newOrigin.x + radius, y: newOrigin.y + radius), radius: radius, startAngle: .degrees(180), delta: .degrees(90))
            
-            newPath.addLine(to: .init(x: xPosition - xConstant, y: newRect.minY))
-            newPath.addLine(to: .init(x: xPosition, y: rect.minY))
-            newPath.addLine(to: .init(x: xPosition + xConstant, y: newRect.minY))
+            path.addLine(to: .init(x: xPosition - xConstant, y: newRect.minY))
+            path.addLine(to: .init(x: xPosition, y: rect.minY))
+            path.addLine(to: .init(x: xPosition + xConstant, y: newRect.minY))
             
-            newPath.addArc(center: .init(x: newRect.maxX - radius, y: newRect.minY + radius), radius: radius, startAngle: .degrees(270), endAngle: .degrees(0), clockwise: false)
+            path.addArc(center: .init(x: newRect.maxX - radius, y: newRect.minY + radius), radius: radius, startAngle: .degrees(270), endAngle: .degrees(0), clockwise: false)
             
-            newPath.addLine(to: .init(x: newRect.maxX, y: newRect.minY + radius))
-            newPath.addLine(to: .init(x: newRect.maxX, y: newRect.maxY - radius))
+            path.addLine(to: .init(x: newRect.maxX, y: newRect.minY + radius))
+            path.addLine(to: .init(x: newRect.maxX, y: newRect.maxY - radius))
             
-            newPath.addRelativeArc(center: .init(x: newRect.maxX - radius, y: newRect.maxY - radius), radius: radius, startAngle: .degrees(0), delta: .degrees(90))
+            path.addRelativeArc(center: .init(x: newRect.maxX - radius, y: newRect.maxY - radius), radius: radius, startAngle: .degrees(0), delta: .degrees(90))
             
-            newPath.addLine(to: .init(x: newRect.minX + radius, y: newRect.maxY))
+            path.addLine(to: .init(x: newRect.minX + radius, y: newRect.maxY))
             
-            newPath.addRelativeArc(center: .init(x: newRect.minX + radius, y: newRect.maxY - radius), radius: radius, startAngle: .degrees(90), delta: .degrees(90))
+            path.addRelativeArc(center: .init(x: newRect.minX + radius, y: newRect.maxY - radius), radius: radius, startAngle: .degrees(90), delta: .degrees(90))
             
-            newPath.addLine(to: .init(x: newRect.minX, y: newRect.minY + radius))
+            path.addLine(to: .init(x: newRect.minX, y: newRect.minY + radius))
         case .bottom(let xMultiplier, let xConstant):
-            let xPosition = CGFloat(round(newRect.maxX - (newRect.maxX * xMultiplier)))
-            newPath.move(to: .init(x: xPosition, y: rect.maxY))
-            newPath.addLine(to: CGPoint(x: xPosition - xConstant, y: newRect.maxY))
-            newPath.addLine(to: CGPoint(x: xPosition + xConstant, y: newRect.maxY))
+            let xPosition = CGFloat(round(newRect.maxX - (newRect.maxX * xMultiplier)) + radius / 2)
+            path.addRelativeArc(center: .init(x: newOrigin.x + radius, y: newOrigin.y + radius), radius: radius, startAngle: .degrees(180), delta: .degrees(90))
+            
+            path.addLine(to: .init(x: newRect.maxX - radius, y: newRect.minY))
+            path.addRelativeArc(center: .init(x: newRect.maxX - radius, y: newRect.minY + radius), radius: radius, startAngle: .degrees(180), delta: .degrees(180))
+            path.addLine(to: .init(x: newRect.maxX, y: newRect.maxY - radius))
+            
+            path.addArc(center: .init(x: newRect.maxX - radius, y: newRect.maxY - radius), radius: radius, startAngle: .degrees(0), endAngle: .degrees(90), clockwise: false)
+            
+            path.addLine(to: CGPoint(x: xPosition + xConstant, y: newRect.maxY))
+            path.addLine(to: .init(x: xPosition, y: rect.maxY))
+            path.addLine(to: CGPoint(x: xPosition - xConstant, y: newRect.maxY))
+   
+            path.addLine(to: .init(x: newRect.minX + radius, y: newRect.maxY))
+            path.addRelativeArc(center: .init(x: newRect.minX + radius, y: newRect.maxY - radius), radius: radius, startAngle: .degrees(90), delta: .degrees(90))
+            path.addLine(to: .init(x: newRect.minX, y: newRect.minY + radius))
         case .leading(let yConstant):
-            newPath.move(to: .init(x: rect.minX, y: newRect.midY))
-            newPath.addLine(to: .init(x: newRect.minX, y: newRect.midY + yConstant))
-            newPath.addLine(to: .init(x: newRect.minX, y: newRect.midY - yConstant))
+            path.move(to: .init(x: rect.minX, y: newRect.midY))
+            path.addLine(to: .init(x: newRect.minX, y: newRect.midY + yConstant))
+            path.addLine(to: .init(x: newRect.minX, y: newRect.midY - yConstant))
         case .trailing(let yConstant):
-            newPath.move(to: .init(x: rect.maxX, y: newRect.midY))
-            newPath.addLine(to: CGPoint(x: newRect.maxX, y: rect.midY + yConstant))
-            newPath.addLine(to: CGPoint(x: newRect.maxX, y: rect.midY - yConstant))
+            path.move(to: .init(x: rect.maxX, y: newRect.midY))
+            path.addLine(to: CGPoint(x: newRect.maxX, y: rect.midY + yConstant))
+            path.addLine(to: CGPoint(x: newRect.maxX, y: rect.midY - yConstant))
         }
-        newPath.closeSubpath()
-        return newPath
+        path.closeSubpath()
+        return path
     }
 }
 
@@ -66,7 +78,7 @@ struct SampleToolTipShape_Previews: PreviewProvider {
             radius: 16,
             toolTipHeight: 16,
             toolWidth: 16,
-            toolTipPosition: .top(xPositionMultiplier: 0.05, 10)
+            toolTipPosition: .bottom(xPositionMultiplier: 0.1, 10)
         )
     }
 }
