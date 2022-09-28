87517c21ddc125b1e462e40bd4385dfe9760d212
Elvis Mwenda <ekithinji@dvt.co.za>
2022-09-28 10:27:03 +0300

    Wrap in a scroll view

--- Shared/DropDown/SampleDropDownView.swift
+++ Shared/DropDown/SampleDropDownView.swift
@@ -4,7 +4,7 @@ struct SampleDropDownView: View {
     @State private var isHidden = true
     @State private var someText: String = ""
     var body: some View {
-        GeometryReader { _ in
+        ScrollView {
             VStack {
                 Spacer().frame(height: 30)
                 ForEach(0 ..< 10) { i in
